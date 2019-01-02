c5cc24d1be7599c80e487a5f04c29a83d5191ff8
Simon Weller <s.meierbeck@gmail.com>
2019-01-02 22:53:51 +0100

    solved day 8 part 1

--- day8/solve1.js
+++ day8/solve1.js
@@ -1,7 +1,37 @@
 module.exports = function (lines) {
   const nodes = parseNodes(lines);
-  const [children, metadataQuantity, ...metadata] = nodes;
-  return metadata.reduce((prev, curr) => prev + curr, 0); 
+  return reduceNodes(nodes);
+}
+
+function reduceNodes(nodes) {
+  const [children, metadataQuantity, ...values] = nodes;
+  const metadata = values.slice(values.length - metadataQuantity);
+  const outerValue = metadata.reduce((prev, curr) => prev + curr, 0);
+
+  let sumOfChildren = 0;
+  let startPosition = 0;
+  for (let childIndex = 0; childIndex < children; childIndex++) {
+    const lengthOfNextChild = nodeLength(values.slice(startPosition));
+    sumOfChildren += reduceNodes(values.slice(startPosition, startPosition + lengthOfNextChild));
+    startPosition += lengthOfNextChild;
+  }
+  return sumOfChildren + outerValue;
+}
+
+function nodeLength(nodes) {
+  const [children, metadataQuantity, ...values] = nodes;
+  if (children === 0) {
+    return metadataQuantity + 2;
+  } else {
+    let totalLength = 2 + metadataQuantity;
+    let remainingValues = [...values];
+    for (let childIndex = 0; childIndex < children; childIndex++) {
+      const childLength = nodeLength(remainingValues);
+      totalLength += childLength;
+      remainingValues = remainingValues.slice(childLength);
+    }
+    return totalLength;
+  }
 }
 
 function parseNodes([str]) {
@@ -9,3 +39,4 @@ function parseNodes([str]) {
 }
 
 module.exports.parseNodes = parseNodes;
+module.exports.nodeLength = nodeLength;
--- day8/test.js
+++ day8/test.js
@@ -1,14 +1,33 @@
 const solve1 = require('./solve1');
-const {parseNodes} = solve1;
+const { parseNodes, nodeLength } = solve1;
 
 describe('solve1', () => {
   it('returns the sum of the metadata entries for a leaf node', () => {
     expect(solve1(['0 1 99'])).toBe(99);
   });
+
+  it('sums up the total of multiple leaf nodes', () => {
+    expect(solve1(['2 0 0 1 10 0 1 20'])).toBe(30);
+  });
+
   it('returns the sum of the metadata entries for a root node and its children', () => {
     expect(solve1(['1 1 0 1 99 2'])).toBe(101);
   });
-})
+
+  it('works with multiple levels of nesting', () => {
+    expect(solve1(['2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'])).toBe(138);
+  });
+});
+
+describe('nodeLength', () => {
+  it('returns the number of elements in the first node', () => {
+    expect(nodeLength([0, 1, 10, 0, 1, 20])).toBe(3);
+  });
+
+  it('includes the length of any child nodes of the first node', () => {
+    expect(nodeLength([1, 1, 0, 1, 30, 10, 0, 1, 20])).toBe(6);
+  });
+});
 
 describe('parseNodes', () => {
   it('parses a string of nodes into an array', () => {
